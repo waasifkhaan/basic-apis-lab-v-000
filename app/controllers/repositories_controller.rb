14336fde6b43339e200248cdda4f06c448b76505
Wasif Khan <waasif.khaan@gmail.com>
2018-11-22 00:19:43 +0000

    automatically backed up by learn

--- app/controllers/repositories_controller.rb
+++ app/controllers/repositories_controller.rb
@@ -1,18 +1,19 @@
 class RepositoriesController < ApplicationController
 
   def search
-    @resp = Faraday.get 'https://application/vnd.github.mercy-preview+json' do |req|
-      req.params['client_id'] = a5ffecadbc9eb6c94d4a
-      req.params['client_secret'] = a063c31e73e2f0c739a05cb60414e358d5251829
-      req.params['v'] = '20160201'
-      
-    end
-    @body_hash = JSON.parse(@resp.body)
-    # @venues = body_hash["response"]["venues"]
-    render 'search'
+    
 
   end
 
   def github_search
+    @resp = Faraday.get 'https://api.github.com/search/repositories' do |req|
+      req.params['client_id'] = a5ffecadbc9eb6c94d4a
+      req.params['client_secret'] = a063c31e73e2f0c739a05cb60414e358d5251829
+      req.params['q'] = params[:query]
+
+    end
+    @body_hash = JSON.parse(@resp.body)
+    # @venues = body_hash["response"]["venues"]
+    render 'search'
   end
 end
