71ef224979a866c04fea44eb6d10632d84f57c3e
Wasif Khan <waasif.khaan@gmail.com>
2018-11-22 00:11:54 +0000

    automatically backed up by learn

--- app/controllers/repositories_controller.rb
+++ app/controllers/repositories_controller.rb
@@ -5,8 +5,7 @@ class RepositoriesController < ApplicationController
       req.params['client_id'] = a5ffecadbc9eb6c94d4a
       req.params['client_secret'] = a063c31e73e2f0c739a05cb60414e358d5251829
       req.params['v'] = '20160201'
-      req.params['near'] = params[:zipcode]
-      req.params['query'] = 'coffee shop'
+      
     end
     @body_hash = JSON.parse(@resp.body)
     # @venues = body_hash["response"]["venues"]
