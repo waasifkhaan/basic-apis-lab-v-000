30fe4bfd9588180af66834c30c0d50a02e21e3be
Wasif Khan <waasif.khaan@gmail.com>
2018-11-22 00:21:21 +0000

    automatically backed up by learn

--- app/controllers/repositories_controller.rb
+++ app/controllers/repositories_controller.rb
@@ -7,8 +7,8 @@ class RepositoriesController < ApplicationController
 
   def github_search
     @resp = Faraday.get 'https://api.github.com/search/repositories' do |req|
-      req.params['client_id'] = a5ffecadbc9eb6c94d4a
-      req.params['client_secret'] = a063c31e73e2f0c739a05cb60414e358d5251829
+      req.params['client_id'] = 'a5ffecadbc9eb6c94d4a'
+      req.params['client_secret'] = 'a063c31e73e2f0c739a05cb60414e358d5251829'
       req.params['q'] = params[:query]
 
     end
